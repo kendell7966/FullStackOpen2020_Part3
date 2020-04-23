3d5b4320c21a7682d1bd09d5d35e757d86b130e6
kendellc <kendell@gmail.com>
2020-04-22 19:06:41 -0700

    uibuild

---
+++ build/precache-manifest.4a3942bb1901be3b1da5611ecac4dc0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169a553133940c900410746f3c2c3284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b46d08c5c0d40485b9",
+    "url": "/static/css/main.34a3a983.chunk.css"
+  },
+  {
+    "revision": "e1c69b87e4574f818aaa",
+    "url": "/static/js/2.693941df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.693941df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b46d08c5c0d40485b9",
+    "url": "/static/js/main.d362924f.chunk.js"
+  },
+  {
+    "revision": "3a7bf3c6e5674494ca12",
+    "url": "/static/js/runtime-main.395da0f9.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json()) // enables "json-parser"
+app.use(express.static('build'))
 
 morgan.token('detail', function getDetail(request) {
     if (request.method != 'POST') {
