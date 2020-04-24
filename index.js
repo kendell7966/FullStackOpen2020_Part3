c235f1e84cac40734c2d0c2286550c241cacab02
kendellc <kendell@gmail.com>
2020-04-23 18:31:48 -0700

    Exercise 3.16
     - Include fix for Exercise 3.14 - allow multiple entries for same name.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Person = require('./models/person')
 
 const app = express()
 //app.use(cors())
-app.use(express.json()) // enables "json-parser"
 app.use(express.static('build'))
+app.use(express.json()) // enables "json-parser"
 
 morgan.token('detail', function getDetail(request) {
     if (request.method != 'POST') {
@@ -47,11 +47,13 @@ app.get('/info', (request, response) => {
     response.send(info)
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        console.log(people)
-        response.json(people.map(person => person.toJSON()))
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(people => {
+            console.log(people)
+            response.json(people.map(person => person.toJSON()))
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -65,28 +67,15 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => {
-            console.log(error.message)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+        .catch(error => next(error))
 })
 
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-const generateId = () => {
-    return getRandomInt(10, 100000)
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -101,22 +90,42 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    let existing = persons.find(person => person.name === body.name)
-    if (existing) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    //let existing = persons.find(person => person.name === body.name)
+    //if (existing) {
+    //    return response.status(400).json({
+    //        error: 'name must be unique'
+    //    })
+    //}
 
     const person = new Person({
         name: body.name,
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson.toJSON())
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
+
+// middleware for handling unsupported routes is next to the last middleware
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+// the error handler middleware is loaded last
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
