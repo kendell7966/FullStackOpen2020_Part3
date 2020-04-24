c008d0d8883538cc6e1889a1b9ebfb069a26883f
kendellc <kendell@gmail.com>
2020-04-24 10:02:10 -0700

    Exercise 3.18

--- index.js
+++ index.js
@@ -18,33 +18,14 @@ morgan.token('detail', function getDetail(request) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :detail'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
-app.get('/info', (request, response) => {
-    let timestamp = new Date()
-    let info = `<p>Phonebook has info for ${persons.length} people</p><p>${timestamp}</p>`
-    response.send(info)
+app.get('/info', (request, response, next) => {
+    Person.find({})
+        .then(people => {
+            let timestamp = new Date()
+            let info = `<p>Phonebook has info for ${people.length} people</p><p>${timestamp}</p>`
+            response.send(info)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
@@ -56,15 +37,16 @@ app.get('/api/persons', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person.toJSON())
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -104,12 +86,6 @@ app.post('/api/persons', (request, response, next) => {
 
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-
-    //if (!body.id) {
-    //    return response.status(400).json({
-    //        error: 'id missing'
-    //    })
-    //}
 
     if (!body.name) {
         return response.status(400).json({
